2ae12b170524f5c0303d37876e10d06f8037d64e
Prakhar <prakhar_au@yahoo.com>
2019-02-10 12:53:44 +0530

    hello string change

--- src/main/java/com/example/javamavenjunithelloworld/Hello.java
+++ src/main/java/com/example/javamavenjunithelloworld/Hello.java
@@ -7,7 +7,7 @@ import java.io.PrintStream;
  */
 public class Hello {
 
-    static final String HELLO = "Ho!!";
+    static final String HELLO = "From dev branch!!";
 
     public static final int MAXIMUM_AMOUNT_OF_TIMES = 20;
 
